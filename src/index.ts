830a28ccb0093096f379f0811d220fc23234d67d
aricma <adrian@aricma.com>
2021-05-23 19:47:43 +0200

    change: fix exports for type declarations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import createLocalStorageActions from './localStorageActions';
+import {BrowserStorageActionsModels} from './models/domain';
+import createLocalStorageActions from './localStorageActions/localStorageActions';
+
+
+export type {
+    BrowserStorageActionsModels,
+};
 
 export {
     createLocalStorageActions,
